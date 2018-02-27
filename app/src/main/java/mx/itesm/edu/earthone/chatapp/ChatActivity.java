3cadd2db23842deb67ed045436b6c6fd0d82efde
earthone <earthone@earthones-MacBook-Pro.local>
2018-02-27 10:45:54 -0600

    Initial commit

---
+++ app/src/main/java/mx/itesm/edu/earthone/chatapp/ChatActivity.java
@@ -0,0 +1,13 @@
+package mx.itesm.edu.earthone.chatapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChatActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
---
+++ app/src/main/java/mx/itesm/edu/earthone/chatapp/MainActivity.java
@@ -0,0 +1,19 @@
+package mx.itesm.edu.earthone.chatapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void enter(View view){
+        
+    }
+}
